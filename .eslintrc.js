be6b73ce74240a49dd4fcc2fecc5f9e8c43614fe
Subhash Doddaga <subhash@Dineshs-MacBook-Pro.local>
2024-01-21 21:01:16 +0530

    test pre commit hook

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+// .eslintrc.js
+
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+  ],
+  rules: {},
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
